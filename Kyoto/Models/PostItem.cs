9e47dddfc0fa283c9c7a0d61e6c58f9872261a3b
Annela Pindis <anpind@ttu.ee>
2019-03-27 11:54:42 +0200

    Models kaust

---
+++ Kyoto/Models/PostItem.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kyoto.Models
+{
+    public class PostItem
+    {
+        [Key]
+        public int ID;
+
+        public string _time;
+
+        public string _date;
+
+        public string _location;
+
+        public string _group;
+
+        public string _heading;
+
+        public string _message;
+    }
+}
